b1d8bac7f12a7a1387ed11fe350e904e7c51d49a
Frostbyte Consulting <126418686+frostbyteconsulting@users.noreply.github.com>
2023-03-13 10:51:32 +1100

    Update imageslider.js

--- javascript/imageslider.js
+++ javascript/imageslider.js
@@ -1,31 +1,53 @@
 const images = document.querySelectorAll(".slider img");
-const indicators = document.querySelectorAll(".slider-indicator");
-
+const indicatorsContainer = document.querySelector(".indicators");
 let currentImg = 0;
 
 function showImage(index) {
   images[currentImg].classList.remove("active");
-  indicators[currentImg].classList.remove("active");
   images[index].classList.add("active");
-  indicators[index].classList.add("active");
   currentImg = index;
 }
 
-function startSlider() {
-  setInterval(() => {
+function createIndicators() {
+  for (let i = 0; i < images.length; i++) {
+    const indicator = document.createElement("button");
+    indicator.addEventListener("click", () => {
+      showImage(i);
+      resetTimer();
+    });
+    indicatorsContainer.appendChild(indicator);
+  }
+}
+
+function setActiveIndicator(index) {
+  const indicators = document.querySelectorAll(".indicators button");
+  for (let i = 0; i < indicators.length; i++) {
+    indicators[i].classList.remove("active");
+  }
+  indicators[index].classList.add("active");
+}
+
+createIndicators();
+setActiveIndicator(0);
+showImage(0);
+
+let timer = setInterval(() => {
+  let index = currentImg + 1;
+  if (index >= images.length) {
+    index = 0;
+  }
+  showImage(index);
+  setActiveIndicator(index);
+}, 5000);
+
+function resetTimer() {
+  clearInterval(timer);
+  timer = setInterval(() => {
     let index = currentImg + 1;
     if (index >= images.length) {
       index = 0;
     }
     showImage(index);
+    setActiveIndicator(index);
   }, 5000);
 }
-
-indicators.forEach((indicator, index) => {
-  indicator.addEventListener("click", () => {
-    showImage(index);
-  });
-});
-
-showImage(0);
-startSlider();
